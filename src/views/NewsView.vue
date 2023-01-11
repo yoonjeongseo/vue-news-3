1434354b8e899da758e0b2137a140138aab0e0c2
seoyoonjeong <yoonnjeong@easymedia.net>
2023-01-11 16:22:57 +0900

    axios를 이용한 데이터 호출

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,11 +1,29 @@
 <template>
   <div>
-    news
+    <div v-for="user in users">{{ user.title }}</div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
+  data() {
+    return {
+      users: []
+    }
+  },
+  created() {
+    var vm = this;
+    axios.get('https://api.hnpwa.com/v0/news/1.json')
+      .then(function(response) {
+        console.log(response);
+        vm.users = response.data
+      })
+      .catch(function(error) {
+        console.log(error);
+      })
+  }
 
 }
 </script>
