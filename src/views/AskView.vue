7a669e5b5408663894ac7d19026ff63d3ebd6ffd
seoyoonjeong <yoonnjeong@easymedia.net>
2023-01-12 10:27:11 +0900

    api함수 구조화 및 데이터출력

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,12 +1,28 @@
 <template>
   <div>
-    ask
+    <div v-for="item in asks">{{ item.title }}</div>
   </div>
 </template>
 
 <script>
-export default {
+import { fetchAskList } from '../api/index.js';
 
+export default {
+  data() {
+    return {
+      asks: []
+    }
+  },
+  created() {
+    var vm = this;
+    fetchAskList()
+      .then(function(response){
+        vm.asks = response.data
+      })
+      .catch(function(error){
+        console.log(error);
+      })
+  }
 }
 </script>
 
--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -1,12 +1,31 @@
 <template>
   <div>
-    job
+    <div v-for="job in jobs">{{ job.title }}</div>
   </div>
 </template>
 
 <script>
-export default {
+import { fetchJobsList } from '../api/index.js';
 
+export default {
+ data() {
+  return {
+    jobs: []
+  }
+ },
+ created() {
+  var vm = this;
+  fetchJobsList()
+    // .then(function(response){
+    //   vm.jobs = response.data
+    // })
+    // .catch(function(error){
+    //   console.log(error);
+    // })
+    .then(response => this.jobs = response.data)
+    .catch(error => console.log(error));
+    //es6 화살표 함수적용
+ }
 }
 </script>
 
