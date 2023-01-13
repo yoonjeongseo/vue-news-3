0daf19ef1cfc673d4b6f28c1087569bef60fa198
seoyoonjeong <yoonnjeong@easymedia.net>
2023-01-13 10:05:27 +0900

    스타일링전에 코드정리

--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -5,27 +5,10 @@
 </template>
 
 <script>
-// import { fetchJobsList } from '../api/index.js';
 
 export default {
-//  data() {
-//   return {
-//     jobs: []
-//   }
-//  },
  created() {
     this.$store.dispatch('FETCH_JOBS');
-//   var vm = this;
-//   fetchJobsList()
-//     // .then(function(response){
-//     //   vm.jobs = response.data
-//     // })
-//     // .catch(function(error){
-//     //   console.log(error);
-//     // })
-//     .then(response => this.jobs = response.data)
-//     .catch(error => console.log(error));
-//     //es6 화살표 함수적용
  }
 }
 </script>
--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -5,26 +5,10 @@
 </template>
 
 <script>
-// import { fetchNewsList } from '../api/index.js';
 
 export default {
-  // data() {
-  //   return {
-  //     users: []
-  //   }
-  // }, //state에서 데이터를 받아오기 떄문에 필요없음
   created() {
     this.$store.dispatch('FETCH_NEWS');
-
-    // var vm = this;
-    // fetchNewsList()
-    //   .then(function(response) {
-    //     console.log(response);
-    //     vm.users = response.data
-    //   })
-    //   .catch(function(error) {
-    //     console.log(error);
-    //   })
   }
 
 }
