5d021cacdca2e7c8edd9587955b672f9a35ae3e0
seoyoonjeong <yoonnjeong@easymedia.net>
2023-02-07 20:41:09 +0900

    사용자 프로필 컴포넌트 공통화 방법1

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,20 +1,24 @@
 <template>
   <div>
-    <p>name : {{ userInfo.id }}</p>
+    <user-profile></user-profile>
+    <!-- <p>name : {{ userInfo.id }}</p>
     <p>karma : {{ userInfo.karma }}</p>
-    <p>created : {{ userInfo.created }}</p>
+    <p>created : {{ userInfo.created }}</p> -->
   </div>
 </template>
 
 <script>
-// import axios from 'axios';
+import UserProfile from "../components/UserProfile.vue";
 
 export default {
-  computed: {
-    userInfo() {
-      return this.$store.state.user;
-    }
+  components:{
+    UserProfile,
   },
+  // computed: {
+  //   userInfo() {
+  //     return this.$store.state.user;
+  //   }
+  // },
   created() {
     // console.log(this.$route.params.id);
     const userName = this.$route.params.id;
